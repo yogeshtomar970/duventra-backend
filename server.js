966ab47d07e2d2e89419f576c50aa65493f1d8c7
yogesh tomar <yogeshtomar970@gmail.com>
2026-04-30 12:42:23 +0530

    fix server

--- server.js
+++ server.js
@@ -65,8 +65,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
-      "http://10.70.73.171:5173",
+      "https://duventra-frontend.vercel.app/"
     ],
     methods: ["GET", "POST"],
   },
