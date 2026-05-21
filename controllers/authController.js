3f5ff2c317db0c3d8481a68b10850318b7682818
yogesh tomar <yogeshtomar970@gmail.com>
2026-05-21 10:11:33 +0530

    authcontroller.js update

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,7 +32,7 @@ export const loginUser = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role },
       process.env.JWT_SECRET,
-      { expiresin:"7d"}
+      { expiresIn:"7d"}
     )
     res.status(200).json({
       success: true,
