4545032c57aa8cde34d5e453f83c30f97c26ee8b
yogesh tomar <yogeshtomar970@gmail.com>
2026-05-21 08:41:34 +0530

    postRoutes add protect route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
+import { protect } from "../middlewares/auth.js";
 import { uploadPost } from "../middlewares/upload.js";
 import { uploadPosts, getAllPosts, increaseViews, updatePost, deletePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
-router.post("/upload", uploadPost.single("file"), uploadPosts);
+router.post("/upload",protect, uploadPost.single("file"), uploadPosts);
 router.get("/all", getAllPosts);
 router.put("/view/:postId", increaseViews);
 
 // ✅ NEW Routes
-router.put("/update/:postId", updatePost);      // POST update (description, formLink)
-router.delete("/delete/:postId", deletePost);   // POST delete
+router.put("/update/:postId",protect, updatePost);      // POST update (description, formLink)
+router.delete("/delete/:postId",protect, deletePost);   // POST delete
 
 export default router;
