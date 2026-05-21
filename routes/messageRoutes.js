df5f73fb9422fdee14323c86bb598e4163713bad
yogesh tomar <yogeshtomar970@gmail.com>
2026-05-21 08:39:20 +0530

    messageRoutes add protect route

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,13 +1,21 @@
 import express from "express";
-import { sendMessage, getConversation, getInbox, searchUsers, deleteMessage, deleteConversation } from "../controllers/messageController.js";
+import { protect } from "../middlewares/auth.js";
+import {
+  sendMessage,
+  getConversation,
+  getInbox,
+  searchUsers,
+  deleteMessage,
+  deleteConversation,
+} from "../controllers/messageController.js";
 
 const router = express.Router();
 
-router.post("/send",                                sendMessage);
-router.get("/inbox/:myId",                          getInbox);
-router.get("/conversation/:myId/:otherId",          getConversation);
-router.delete("/conversation/:myId/:otherId",       deleteConversation);
-router.get("/search",                               searchUsers);
-router.delete("/:messageId",                        deleteMessage);
+router.post("/send",protect, sendMessage);
+router.get("/inbox/:myId",protect, getInbox);
+router.get("/conversation/:myId/:otherId",protect, getConversation);
+router.delete("/conversation/:myId/:otherId",protect, deleteConversation);
+router.get("/search", searchUsers);
+router.delete("/:messageId", protect,deleteMessage);
 
 export default router;
