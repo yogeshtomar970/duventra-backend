2519b4f488e5f6b4ac2e09c7023c6dfcb45a0760
yogesh tomar <yogeshtomar970@gmail.com>
2026-06-20 10:44:45 +0530

    authroutes.js forgotPassword,verifyOtp,resetPassword, add

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { loginUser } from "../controllers/authController.js";
+import {
+  loginUser,
+  forgotPassword,
+  verifyOtp,
+  resetPassword,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp", verifyOtp);
+router.post("/reset-password", resetPassword);
 
 export default router;
