bf579f94ed056ddd872a1253a08fe9b72a7d5663
yogesh tomar <yogeshtomar970@gmail.com>
2026-06-30 08:46:49 +0530

    authroutes.js update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
 import {
   loginUser,
-  checkEmail,
+  forgotPassword,
+  verifyOtp,
   resetPassword,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
-router.post("/check-email", checkEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp", verifyOtp);
 router.post("/reset-password", resetPassword);
 
 export default router;
