b609fe6c91598f60528b75d7113043ea8d17f76c
yogesh tomar <yogeshtomar970@gmail.com>
2026-06-30 10:33:02 +0530

    studentcontroller.js update

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -49,7 +49,6 @@ export const studentSignup = async (req, res) => {
         normalize(v.collegeName) === normalize(collegeName),
     );
 
-
   
     if (!isValid) {
       return res.status(403).json({
