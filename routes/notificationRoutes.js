924255d725ea4c0beade7e6ec0b27772d42ba12d
yogesh tomar <yogeshtomar970@gmail.com>
2026-08-01 09:34:04 +0530

    notificationroutes.js update

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -7,14 +7,15 @@ import {
   deleteSelected,
   deleteAll,
 } from "../controllers/notificationController.js";
+import { protect } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/unread/:recipientId", getUnreadCount);
-router.put("/read-all", markAllRead);
-router.put("/read/:id", markOneRead);
-router.delete("/delete-selected", deleteSelected);
-router.delete("/delete-all", deleteAll);
-router.get("/:recipientId", getNotifications);
+router.get("/unread/:recipientId", protect, getUnreadCount);
+router.put("/read-all", protect, markAllRead);
+router.put("/read/:id", protect, markOneRead);
+router.delete("/delete-selected", protect, deleteSelected);
+router.delete("/delete-all", protect, deleteAll);
+router.get("/:recipientId", protect, getNotifications);
 
 export default router;
