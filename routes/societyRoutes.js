b549d34de437ad396580fa29a6952c98c57dd44a
yogesh tomar <yogeshtomar970@gmail.com>
2026-08-01 09:33:25 +0530

    societyroutes.js update

--- routes/societyRoutes.js
+++ routes/societyRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { uploadProfilePic } from "../middlewares/upload.js";
+import { protect } from "../middlewares/auth.js";
 import { societySignup, verifySocietyEmail, getSocietyProfile, getSocietyPublicProfile, updateSocietyProfile, addCommitteeMember, removeCommitteeMember } from "../controllers/societyController.js";
 
 const router = express.Router();
@@ -8,8 +9,8 @@ router.post("/verify-email", verifySocietyEmail);
 router.post("/signup", societySignup);
 router.get("/public/:societyId", getSocietyPublicProfile);
 router.get("/profile/:id", getSocietyProfile);
-router.put("/update/:id", uploadProfilePic.single("profilePic"), updateSocietyProfile);
-router.post("/committee/:id", addCommitteeMember);
-router.delete("/committee/:id", removeCommitteeMember);
+router.put("/update/:id", protect, uploadProfilePic.single("profilePic"), updateSocietyProfile);
+router.post("/committee/:id", protect, addCommitteeMember);
+router.delete("/committee/:id", protect, removeCommitteeMember);
 
 export default router;
